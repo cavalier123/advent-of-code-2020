60e97adec0c3478fef168e3732903c0478cd6dac
Ray Evans <ray.evans@experian.com>
2020-12-02 08:45:54 +0000

    Reformatted

--- src/main/kotlin/uk/co/rge123/solutions/DayTwoPartOneSolution.kt
+++ src/main/kotlin/uk/co/rge123/solutions/DayTwoPartOneSolution.kt
@@ -5,18 +5,18 @@ import main.kotlin.uk.co.rge123.utils.splitFileToLines
 fun main() {
     val filename = "DayTwoData"
     val solution = solveItDayTwoPartOne(splitFileToLines(filename))
-    print ("Solution is $solution")
+    print("Solution is $solution")
 }
 
-fun solveItDayTwoPartOne(lines: List<String>)= lines.filter{getPasswordInfo(it).isValid()}.count()
+fun solveItDayTwoPartOne(lines: List<String>) = lines.filter { getPasswordInfo(it).isValid() }.count()
 
 fun getPasswordInfo(input: String): PasswordInfo {
     val words = input.split(" ")
     val rangeNumbers = words[0].split("-")
-    return PasswordInfo(rangeNumbers[0].toInt(), rangeNumbers[1].toInt() ,words[1][0], words[2])
+    return PasswordInfo(rangeNumbers[0].toInt(), rangeNumbers[1].toInt(), words[1][0], words[2])
 }
 
 data class PasswordInfo(val rangeStart: Int, val rangeEnd: Int, val checkedChar: Char, val password: String) {
-    fun isValid() = password.count{it == checkedChar} in rangeStart .. rangeEnd
-    fun isValidPartTwo() = (password[rangeStart -1] == checkedChar).xor(password[rangeEnd -1] == checkedChar)
+    fun isValid() = password.count { it == checkedChar } in rangeStart..rangeEnd
+    fun isValidPartTwo() = (password[rangeStart - 1] == checkedChar).xor(password[rangeEnd - 1] == checkedChar)
 }
--- src/main/kotlin/uk/co/rge123/solutions/DayTwoPartTwoSolution.kt
+++ src/main/kotlin/uk/co/rge123/solutions/DayTwoPartTwoSolution.kt
@@ -5,7 +5,7 @@ import main.kotlin.uk.co.rge123.utils.splitFileToLines
 fun main() {
     val filename = "DayTwoData"
     val solution = solveItDayTwoPartTwo(splitFileToLines(filename))
-    print ("Solution is $solution")
+    print("Solution is $solution")
 }
 
-fun solveItDayTwoPartTwo(lines: List<String>) = lines.filter{getPasswordInfo(it).isValidPartTwo()}.count()
+fun solveItDayTwoPartTwo(lines: List<String>) = lines.filter { getPasswordInfo(it).isValidPartTwo() }.count()
