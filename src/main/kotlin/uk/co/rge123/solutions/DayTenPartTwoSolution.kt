314925e5b5f86e40956f9fae682e5a4217769c7f
Ray Evans <ray.evans@experian.com>
2020-12-10 18:00:14 +0000

    Minor change

--- src/main/kotlin/uk/co/rge123/solutions/DayTenPartTwoSolution.kt
+++ src/main/kotlin/uk/co/rge123/solutions/DayTenPartTwoSolution.kt
@@ -9,14 +9,13 @@ fun main() {
 }
 
 fun solveItDayTenPartTwo(numbers: List<Int>): Long {
-    val hash = HashMap<Int, Long>()
-    hash.put(157, 1L)
+    val hash = mutableMapOf(Pair(157, 1L))
     for (index in numbers.size - 2 downTo 0) {
         hash[numbers[index]] = getTotalRoutesFromChildren(numbers[index], hash)
     }
     return getTotalRoutesFromChildren(0, hash)
 }
 
-fun getTotalRoutesFromChildren(startNum: Int, hash: HashMap<Int, Long>): Long {
+fun getTotalRoutesFromChildren(startNum: Int, hash: MutableMap<Int, Long>): Long {
     return (startNum + 1 .. startNum + 3).fold(0L){ r, t -> r + (hash[t] ?: 0) }
 }
